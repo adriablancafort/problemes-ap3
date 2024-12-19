81ae8f577f025d80185604262226d963d54bddee
Adrià Blancafort <adriablancafort@gmail.com>
2024-12-19 23:45:42 +0100

    optimize

--- dynamic-programing/adding-subrectangles.cpp
+++ dynamic-programing/adding-subrectangles.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-const int MAX_NUM = 67;
+const int MAX_NUM = 300;
 const int UNDEF = -1;
 
-int rec(int n, vector<int> cache) {
+int rec(int n, vector<int>& cache) {
     if (cache[n] != UNDEF) return cache[n];
 
     if (n == 1) {
@@ -22,13 +23,13 @@ int letterToIndex(char letter) {
 
 int main() {
     int sum = 0;
-    int inp;
     char letter;
     vector<int> cache(MAX_NUM + 1, UNDEF);
 
-    while(cin >> letter) {
-        inp = letterToIndex(letter);
+    while (cin >> letter) {
+        int inp = letterToIndex(letter);
         sum += rec(inp, cache);
         cout << sum << endl;
     }
+    return 0;
 }
